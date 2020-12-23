78601557f5cdd158fe00731f501929d8cb8fb40e
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-12-23 15:58:24 +0000

    Added JS functionality to add and remove hidden class on modal and overlay

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', function() {
+        console.log('button clicked')
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
 }
+
+btnCloseModal.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+overlay.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
