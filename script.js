e5d21b544ac69da8105035d9d3aeb3e54defda69
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-12-23 16:18:04 +0000

    Added Escape keydown event to hide modal when open

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// e typically stands for event
+document.addEventListener('keydown', function (e) {
+    // Log of the key pressed
+    console.log(e.key);
+
+    // If the key is equal to escape and the modal classlist does not contain hidden, run closeModal
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
